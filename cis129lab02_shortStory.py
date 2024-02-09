a55bef6e1c2f0bc975850fcda0751c6edb441ef6
Jperez2799 <159219792+Jperez2799@users.noreply.github.com>
2024-02-08 19:10:17 -0700

    Update cis129lab02_shortStory.py
    I added my name and a description of what is was printing
    i then proceeded to tell a short story using print statments

--- cis129lab02_shortStory.py
+++ cis129lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World')
+# Author: Joel Perez
+# My first python lab in course CIS219
+#prints a simple short story
+print('this morning i woke up')
+print('i was exuasted')
+print(' i drank coffee to help')
+print('it was black coffee no sugar')
+print('it tatsed awful but it woke me up')
+
